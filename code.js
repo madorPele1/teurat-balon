cc189373d5efaab64594f2fc510a747802d6eaf1
madorPele1 <159686722+madorPele1@users.noreply.github.com>
2025-11-23 14:14:40 +0200

    Add files via upload

--- code.js
+++ code.js
@@ -6,11 +6,24 @@ let btn;
 let btnNextPart;
 let titleParts;
 let clickStage1 = 0; // משתנה גלובלי לניהול הסדר
+let motHarama;
+let arrow;
+let wire;
+
+// function addWire() {
+//   if (!document.querySelector(".wire")) {
+//     // לבדוק אם כבר קיים
+//     const wire = document.createElement("img");
+//     wire.src = "media/wire.png";
+//     wire.classList.add("wire");
+//     document.body.appendChild(wire);
+//   }
+// }
 
 window.addEventListener("load", () => {
   document.getElementById("startButton").addEventListener("click", function () {
     // החלפת רקע
-    document.body.style.backgroundImage = "url('media/navpage.svg')";
+    document.body.style.backgroundImage = "url('media/backgroundRegular.svg')";
 
     // מחיקת כפתור
     this.style.display = "none";
@@ -20,6 +33,10 @@ window.addEventListener("load", () => {
   });
 });
 function navbar(level) {
+  // addWire();
+  // -------------------------------
+  // הוספת החוט מיד מעל הנורות
+  // -------------------------------
   const bulbs = document.getElementsByClassName("bulb");
 
   // הצגת כל הנורות + איפוס קלאס "on"
@@ -58,6 +75,10 @@ function navbar(level) {
   // מאזיני לחיצה עם סדר לפי clickStage
   const img1 = document.getElementById("img1");
   const img2 = document.getElementById("img2");
+  const wire1 = document.getElementById("wire");
+  wire1.src = "media/wire.png";
+
+  wire1.style.visibility = "visible";
 
   // מחיקה של מאזינים קיימים כדי למנוע כפילויות
   img1.onclick = null;
@@ -69,19 +90,21 @@ function navbar(level) {
       document.body.style.backgroundImage = "url('media/backgroung2.png')";
       container.style.display = "none";
       hideBulbs();
-      clickStage++; // מעבר לשלב הבא
+      clickStage++;
     }
+    wire1.style.visibility = "hidden";
   };
 
   img2.onclick = () => {
     if (clickStage === 1) {
-      // מופעל רק אחרי img1
-      step2();
+      step2New();
+      wire1.style.visibility = "hidden";
+
       document.body.style.backgroundImage =
         "url('media/backgroundRegular.svg')";
       container.style.display = "none";
       hideBulbs();
-      clickStage++; // אם יש שלבים נוספים
+      clickStage++;
     }
   };
 }
@@ -107,7 +130,7 @@ function step1() {
   title1.textContent = "נתונים טכניים";
   title1.classList.add("textTitle");
   title1.style.top = "40%";
-  title1.style.right = "35%";
+  title1.style.right = "30%";
 
   document.body.appendChild(title1);
 
@@ -149,7 +172,7 @@ function step1() {
   document.body.appendChild(imgBalon);
 
   btn.addEventListener("click", function () {
-    document.body.style.backgroundImage = "url('media/navpage.svg')";
+    document.body.style.backgroundImage = "url('media/backgroundRegular.svg')";
 
     this.style.visibility = "hidden";
 
@@ -159,13 +182,16 @@ function step1() {
     table.style.display = "none";
     imgBalon.style.visibility = "hidden";
 
-    document.getElementById("text-container").style.display = "block";
-    const elements = document.getElementsByClassName("bulb");
-    for (let el of elements) {
-      el.style.display = "block";
-    }
-    level = level + 1;
-    navbar(level);
+    // document.getElementById("text-container").style.display = "block";
+    // const elements = document.getElementsByClassName("bulb");
+    // for (let el of elements) {
+    //   el.style.display = "block";
+    // }
+    step2();
+    document.body.style.backgroundImage = "url('media/backgroundRegular.svg')";
+
+    // level = level + 1;
+    // navbar(level);
   });
 }
 
@@ -184,19 +210,42 @@ function step2() {
     titleParts.classList.add("title-part-style");
     document.body.appendChild(titleParts);
   }
+  //   btnClickOnme = document.createElement("p");
+  //   btnClickOnme.textContent = "לחצו עליי";
+  //   btnClickOnme.classList.add("click-on-me");
+  //   btnClickOnme.style.right = "10vw";
+  // //   btnClickOnme.style.top = "10vh";
 
-  imgBalon.style.cssText = `
-    visibility: visible;
-    width: 70vw;
-    height: 60vh;
-    position: absolute;
-    right: 12%;
-    bottom: 16%;
-  `;
+  //   btnClickOnme.style.paddingRight = "20px";
+  //   btnClickOnme.style.paddingLeft = "20px";
+  //   document.body.appendChild(btnClickOnme);
 
-  imgBalon.addEventListener("click", function () {
+  const lightBalon = document.createElement("img");
+  lightBalon.src = "media/lightBalon.png";
+  lightBalon.classList.add("light-Balon");
+  document.body.appendChild(lightBalon);
+
+  //   imgBalon.style.cssText = `
+  //     visibility: visible;
+  //     width: 70vw;
+  //     height: 60vh;
+  //     position: absolute;
+  //     right: 12%;
+  //     bottom: 16%;
+  //   `;
+
+  lightBalon.addEventListener("click", function () {
     titleParts.textContent = "מוט ההרמה"; // עכשיו titleParts גלובלי
     parts();
+    lightBalon.style.visibility = "hidden";
+    motHarama = document.createElement("img");
+    motHarama.src = "media/motHarama.svg";
+    motHarama.classList.add("motHarama");
+    document.body.appendChild(motHarama);
+    arrow = document.createElement("img");
+    arrow.src = "media/arrow.svg";
+    arrow.classList.add("arrowPart1");
+    document.body.appendChild(arrow);
   });
 }
 
@@ -228,28 +277,82 @@ function parts() {
   btnNextPart.style.paddingLeft = "20px";
   document.body.appendChild(btnNextPart);
 
-  // הסרת מאזינים קודמים אם קיימים
+  // הסרת מאזינים קודמים
   btnNextPart.onclick = null;
 
   btnNextPart.onclick = function () {
+    motHarama.style.display = "none";
+
     if (clickStage1 === 0) {
       titleParts.textContent = "חצובה";
       titleParts.style.right = "38%";
       textPart.textContent =
         "החצובה מורכבת משלושה רגליי ייצוב עם פרפריות לצורך חיזוק ואיזון החצובה";
+      arrow.classList.remove("arrowPart1");
+      arrow.classList.add("arrowPart2");
       clickStage1++;
     } else if (clickStage1 === 1) {
       titleParts.textContent = "גוף התאורה";
       titleParts.style.right = "30%";
       textPart.textContent =
         "גוף התאורה הוא נורת לד המוקפת במעטפת עגולה בצורת בלון";
+      arrow.classList.remove("arrowPart2");
+      arrow.classList.add("arrowPart3");
       clickStage1++;
     } else if (clickStage1 === 2) {
       titleParts.textContent = "משקולת";
       titleParts.style.right = "35%";
-      textPart.textContent =
-        "המשקולות מאזנות את החצובה, ישנן 3 משקולות";
+      textPart.textContent = "המשקולות מאזנות את החצובה, ישנן 3 משקולות";
+      arrow.classList.remove("arrowPart3");
+      arrow.classList.add("arrowPart4");
       clickStage1++;
+
+      btnNextPart.textContent = "בחזרה למסלול";
+      btnNextPart.style.right = "27%";
+
+      // מסיר את ה-onclick הראשי כדי שלא ירוץ פעמיים
+      btnNextPart.onclick = null;
+
+      btnNextPart.addEventListener("click", function () {
+        document.body.style.backgroundImage =
+          "url('media/backgroundRegular.svg')";
+
+        this.style.visibility = "hidden";
+
+        titleParts.style.display = "none";
+        arrow.style.display = "none";
+        btnNextPart.style.visibility = "hidden";
+        imgBalon.style.display = "none";
+        textPart.style.display = "none";
+
+        document.getElementById("text-container").style.display = "block";
+        const elements = document.getElementsByClassName("bulb");
+        for (let el of elements) {
+          el.style.display = "block";
+        }
+
+        // document.body.style.backgroundImage =
+        //   "url('media/backgroundRegular.svg')";
+
+        level = 1;
+
+        navbar(level);
+      });
     }
   };
+}
+
+function step2New() {
+  const title = document.createElement("h2");
+  title.classList.add("Title");
+  title.textContent = "הכנות ובדיקות לפני הפעלה";
+  document.body.appendChild(title);
+
+  const textPrep = document.createElement("p");
+  textPrep.classList.add("text-prep");
+  textPrep.textContent = "נוודא כי המעטפת יבשה לחלוטין לפני התקנת הכיסוי המגן";
+  document.body.appendChild(textPrep);
+  btnNextPart.style.visibility = "visible";
+  btnNextPart.style.right = "40vw";
+  btnNextPart.textContent = "המשך";
 }
